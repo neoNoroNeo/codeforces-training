c57ccb0b9fa88aa7479c262ec7b6b8467ad3a481
neoNoroNeo <mhaniifmuzaki@gmail.com>
2024-02-27 04:57:33 +0700

    Watermelon
    Link: https://codeforces.com/problemset/problem/4/A

---
+++ Watermelon/watermelon.py
@@ -0,0 +1,8 @@
+print("Input semangka yang ingin kamu belah")
+jumlah_semangka = int(input())
+remainder_semangka = jumlah_semangka % 2
+
+if remainder_semangka == 1:
+    print("NO")
+else:
+    print("YES")
